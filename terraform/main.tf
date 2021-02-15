ad0c5c6eba17ea1a6269bff39413d3d474898e54
Xavier Salazar <x.salazar1729@gmail.com>
2021-02-14 18:25:44 -0800

    Add S3 state backend to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,14 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "s3" {
+    bucket = "xsalazar-terraform-state"
+    key    = "aws-docker/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
 output api_endpoint {
   value = aws_api_gateway_deployment.instance.invoke_url
 }
